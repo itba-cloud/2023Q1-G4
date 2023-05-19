ed15f907fb13637875ee75a57ac2f221f1997637
Salus Zavalía <szavalia@itba.edu.ar>
2023-05-19 19:13:23 -0300

    Fixed API GW integration
    Co-authored-by: Lucas Sebastian Gomez <lugomez@itba.edu.ar>

--- modules/api_gw/main.tf
+++ modules/api_gw/main.tf
@@ -21,10 +21,10 @@ resource "aws_api_gateway_method" "this" {
 
 resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.this.id
+  resource_id             = aws_api_gateway_method.this.resource_id
   http_method             = aws_api_gateway_method.this.http_method
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = var.lambda_func_arn
 }
 
@@ -47,4 +47,12 @@ resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.this.id
   stage_name    = var.rest_api_stage_name
+}
+
+resource "aws_lambda_permission" "this" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_func_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn   = "${aws_api_gateway_rest_api.this.execution_arn}/*/${aws_api_gateway_method.this.http_method}/${aws_api_gateway_resource.this.path_part}"
 }
