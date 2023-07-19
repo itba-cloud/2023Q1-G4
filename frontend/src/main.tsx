e7c00f419e8a6b0f2a761728319de36f07286bf6
Gian <gpecile@itba.edu.ar>
2023-07-18 23:27:45 -0300

    chore: delete about

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import {rootRoute} from './App.tsx'
 import {Router, RouterProvider} from "@tanstack/router";
 import {indexRoute} from "@/pages/Index.tsx";
-import {aboutRoute} from "@/pages/About.tsx";
 import {dailiesRoute, dailyRoute} from "@/pages/Daily.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {loginRoute} from "@/pages/Login.tsx";
@@ -14,7 +13,7 @@ import {dashboardRoute} from "@/pages/Dashboard.tsx";
 
 // Create the route tree using your routes
 const routeTree = rootRoute.addChildren(
-    [indexRoute, aboutRoute, dailiesRoute.addChildren([dailyRoute]), loginRoute, registerRoute, confirmRegistrationRoute, dashboardRoute]
+    [indexRoute, dailiesRoute.addChildren([dailyRoute]), loginRoute, registerRoute, confirmRegistrationRoute, dashboardRoute]
 )
 
 // Create the router using your route tree
@@ -34,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router}/>
-            <Toaster />
+            <Toaster/>
         </QueryClientProvider>
     </React.StrictMode>,
 )
--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,9 +1,9 @@
 // Create an index route
-import { Route, useNavigate } from "@tanstack/router";
-import { rootRoute } from "@/App.tsx";
-import { cn } from "@/lib/utils.ts";
-import { Button } from "@/components/ui/button.tsx";
-import { toast } from "@/components/ui/use-toast.ts";
+import {Route, useNavigate} from "@tanstack/router";
+import {rootRoute} from "@/App.tsx";
+import {cn} from "@/lib/utils.ts";
+import {Button} from "@/components/ui/button.tsx";
+import {toast} from "@/components/ui/use-toast.ts";
 import "../assets/gradient.css"
 
 export const indexRoute = new Route({
@@ -12,60 +12,8 @@ export const indexRoute = new Route({
     component: Index,
 })
 
-/*
-const DUMMY_USERS: User[] = [
-    {
-        id: 1234,
-        name: 'Miguel OHara',
-        email: 'mohara@protonmail.com',
-        role: 'Scrum Master',
-        team: 'Miguel OHara',
-    },
-    {
-        id: 123456,
-        name: 'Gwen Stacy',
-        email: 'gstacy@protonmail.com',
-        role: 'Programmer',
-        team: 'Miguel OHara',
-    },
-    {
-        id: 1234567,
-        name: 'Jessica Drew',
-        email: 'jdrew@protonmail.com',
-        role: 'Programmer',
-        team: 'Miguel OHara',
-    }
-]
-
-const DUMMY_DAILIES: Daily[] = [
-    {
-        id: 123,
-        date: LocalDate.now(),
-        user: DUMMY_USERS[0],
-        yesterday: 'Yesterday I did some stuff',
-        today: 'Today I will do some stuff',
-        blockers: 'I have no blockers',
-    },
-    {
-        id: 1234,
-        date: LocalDate.now(),
-        user: DUMMY_USERS[1],
-        yesterday: 'Yesterday I hung out with Miles',
-        today: 'Today I won\'t hang out with Miles',
-        blockers: 'Yes, Miles is a blocker',
-    },
-    {
-        id: 12345,
-        date: LocalDate.now(),
-        user: DUMMY_USERS[2],
-        yesterday: 'Yesterday I trained with Gwen',
-        today: 'Today I will train with Ben',
-        blockers: 'I have no blockers',
-    }
-]*/
-
 function Index() {
-    const navigate = useNavigate({ from: '/' })
+    const navigate = useNavigate({from: '/'})
 
     function handleRedirect(to: string) {
         navigate({
@@ -80,22 +28,20 @@ function Index() {
 
     return (
         <div className="vaporwave-gradient min-h-screen flex items-center justify-center">
-      <div className="container">
-        <div className="grid md:grid-cols-2 gap-6 min-h-screen items-center ">
-          <div className={cn("shadow-lg p-6 space-y-4 border-2 rounded-2xl dark:bg-black bg-white flex flex-col justify-center items-center h-96")}>
-            <h1 className="text-4xl font-bold text-left">
-              Are you a PM?
-            </h1>
-            <Button className="w-fit" onClick={() => handleRedirect('dashboard')}>Dashboard</Button>
-          </div>
-          <div className={cn("shadow-lg p-6 space-y-4 border-2 rounded-2xl dark:bg-black bg-white flex flex-col justify-center items-center h-96")}>
-            <h1 className="text-4xl font-bold text-left">
-              Are you a developer?
-            </h1>
-            <Button className="w-fit" onClick={() => handleRedirect('')}>Dailies</Button>
-          </div>
+            <div className="container grid md:grid-cols-2 gap-8 min-h-screen items-center">
+                <div className={cn("shadow-lg p-6 space-y-4 border-2 rounded-3xl dark:bg-black bg-white flex flex-col justify-center items-center h-96")}>
+                    <h1 className="text-4xl font-bold text-center">
+                        Are you a PM?
+                    </h1>
+                    <Button className="w-fit" onClick={() => handleRedirect('dashboard')}>Dashboard</Button>
+                </div>
+                <div className={cn("shadow-lg p-6 space-y-4 border-2 rounded-3xl dark:bg-black bg-white flex flex-col justify-center items-center h-96")}>
+                    <h1 className="text-4xl font-bold text-center">
+                        Are you a developer?
+                    </h1>
+                    <Button className="w-fit" onClick={() => handleRedirect('')}>Dailies</Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     );
 }
