c0e59d00f6fe85b927912c92a1cb3ba741a269fd
Gian <gpecile@itba.edu.ar>
2023-07-18 21:48:30 -0300

    fix: subscription lambda
    Co-authored-by: Matías Federico Lombardi <matiaslombardi@users.noreply.github.com>

--- resources/lambda/subscriptions/post/index.js
+++ resources/lambda/subscriptions/post/index.js
@@ -25,11 +25,43 @@ exports.handler = async (event, context) => {
   team = result.rows[0]
 
   const topic = await client.createTopic({ Name: team.name }).promise();
+
+  const subscriptionsList = await client.listSubscriptionsByTopic({ 
+    TopicArn: topic.TopicArn
+  }).promise();
+
+
+  if (!subscriptionsList.Subscriptions) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST'
+      },
+      body: JSON.stringify({ message: 'Could not retrieve subscriptions' })
+    }
+  }
+
+  for (subscription of subscriptionsList.Subscriptions) {
+
+    if (subscription.Endpoint === body.email) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST'
+        },
+        body: JSON.stringify({ message: 'Already subscripted' })
+      }
+    }
+  }
+
   res = await client.subscribe({
     TopicArn: topic.TopicArn,
     Protocol: 'email',
     Endpoint: body.email
   }).promise();
+
 
   return {
     statusCode: 200,
